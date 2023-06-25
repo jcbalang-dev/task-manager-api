4e2220fdf152fecfbf1bad10a5f72f0fe9c2d010
John Christian Balang <dev.jcbalang@gmail.com>
2023-06-25 13:10:14 +0800

    convert port number to actual number

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from dotenv import dotenv_values
 env = dotenv_values()
 
 # environment variables
-api_port = env['PORT_NUMBER']
+api_port = int(env['PORT_NUMBER'])
 
 app = Flask(__name__)
 
-if __name__='__main__':
+if __name__=='__main__':
     app.run( host='0.0.0.0', port = api_port )
