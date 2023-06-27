4d1bc5067a7b4d41704f52fa272de8bae4dfea9b
John Christian Balang <jcbalang-dev@gmail.com>
2023-06-28 04:04:13 +0800

    add ssh client and connection to app run

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from dotenv import dotenv_values
 import mysql.connector
+import paramiko
 
 # Load environment variables from .env 
 env = dotenv_values()
@@ -15,15 +16,57 @@ db_user = env['DB_USER']
 db_password = env['DB_PASSWORD']
 db_database = env['DB_DATABASE']
 
+#mysql ssh connection
+mysql_port = int(env['MYSQL_PORT'])
+use_ssh = bool(env.get('USE_SSH','False'))
+
+ssh_host = env['SSH_HOST']
+ssh_username = env['SSH_USERNAME']
+ssh_private_key_path = env['SSH_PRIVATE_KEY_PATH']
+
+
 app = Flask(__name__)
 
 # MySQL connection
-db_connection = mysql.connector.connect(
-    host = db_host ,
-    user = db_user ,
-    password = db_password ,
-    database = db_database
-)
+if use_ssh:
+    # create ssh client
+    ssh_client = paramiko.SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # establish ssh connection
+    ssh_client.connect(
+        ssh_host ,
+        username = ssh_username ,
+        key_filename = ssh_private_key_path
+    )
+
+    # create an ssh tunnel and forward the MySQL port
+    ssh_tunnel = ssh_client.get_transport().open_channel('direct-tcpip', (db_host, mysql_port), ('localhost', mysql_port))
+
+    #connection using SSH
+    db_connection = mysql.connector.connect(
+        host = '127.0.0.1' ,
+        port = mysql_port ,
+        user = db_user ,
+        password = db_password ,
+        database = db_database ,
+        unix_socket = '',
+        ssl_disabled = True ,
+        use_pure = True ,
+        connection_time_out = 30 ,
+        allow_local_infile = True ,
+        ssl_verify_cert = False ,
+        ssh = ssh_tunnel
+    )
+else:
+    # connection without SSH
+    db_connection = mysql.connector.connect(
+        host = db_host ,
+        user = db_user ,
+        password = db_password ,
+        database = db_database
+    )
 
 # create a cursor
 db_cursor = db_connection.cursor()
@@ -35,3 +78,7 @@ if __name__=='__main__':
 # close MySQL cursor and connection
 db_cursor.close()
 db_connection.close()
+
+# close SSH connection
+if use_ssh:
+    ssh_client.close()
